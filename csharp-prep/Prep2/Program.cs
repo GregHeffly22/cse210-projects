249225100cd466dfbc378dc70f06ce1d9dfcacb1
John Doe <eva21010@byui.edu>
2023-04-22 12:32:09 -0600

    Prep 1 + 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your percentage in the class?");
+        string percentageString = Console.ReadLine();
+        char letter = 'F';
+        char sign =' ';
+        double percentage = Convert.ToDouble(percentageString);
+        
+        
+        if (percentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (percentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (percentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (percentage >= 60)
+        {
+            letter = 'D';
+        }
+        else 
+        {
+            letter = 'F';
+        }
+        
+        
+        if (percentage >=  70)
+        {
+            Console.Write("You have passed! Congratulations!");
+        }
+        else
+        {
+            Console.Write("You have Failed. Please Try again!");
+        }
+
+        if (percentage >= 93)
+        {
+            sign = ' ';
+        }
+        else if (percentage < 60)
+        {
+            sign = ' ';
+        }
+        else if (percentage%10 < 3)
+        {
+            sign ='-';
+        }
+        else if (percentage%10 >= 6)
+        {
+            sign ='+';
+        }
+
+
+
+        Console.WriteLine($" You received a {letter}{sign}.");
     }
-}
+    
+    }
